4c226e374b33daf79953b65ee48ce4b3ac23c7a8
jjhosuespinoza <esp21017@byui.edu>
2022-02-03 22:01:16 +0100

    commit

--- js/bom.js
+++ js/bom.js
@@ -1,4 +1,4 @@
-const list = document.querySelector('ul');
+const list = document.querySelector('#list');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
